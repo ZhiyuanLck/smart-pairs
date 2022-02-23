5f3ea0f00b78b08c01140cd4cbaa2aa9cf7e6730
ZhiyuanLck <lichangkai225@qq.com>
2022-02-23 15:44:53 +0800

    fix #23: some options are not merged

--- lua/pairs/init.lua
+++ lua/pairs/init.lua
@@ -268,7 +268,9 @@ function Pairs:setup(opts)
   u.merge(self.space, opts.space)
   u.merge(self.enter, opts.enter)
   u.merge(self.indent, opts.indent)
+  u.merge(self.mapping, opts.mapping)
   u.merge(self.autojump_strategy, opts.autojump_strategy)
+  u.merge(self.max_search_lines, opts.max_search_lines)
 
   for ft, pairs in pairs(opts.pairs or {}) do
     self.pairs[ft] = pairs
