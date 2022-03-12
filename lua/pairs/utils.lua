52c75e0984edcdc7b43d5f7259540b38d8d0c02c
ZhiyuanLck <lichangkai225@qq.com>
2022-03-12 16:02:05 +0800

    refactor: add utils function check_type

--- lua/pairs/utils.lua
+++ lua/pairs/utils.lua
@@ -11,4 +11,15 @@ function M.warn(msg)
   ]], msg:gsub('"', '\\"')))
 end
 
+--- check the type of variable
+---@param var any @variable of any type
+---@param expect_type string @expected type of the variable
+---@param allow_nil boolean @allow the variable to be nil, default false
+function M.check_type(var, expect_type, allow_nil)
+  local var_type = type(var)
+  if (not allow_nil or val ~= nil) and var_type ~= expect_type then
+    error(fmt('expect type %s, but get type %s', expect_type, var_type))
+  end
+end
+
 return M
---
+++ tests/pairs/utils_spec.lua
@@ -0,0 +1,28 @@
+local u = require('pairs.utils')
+
+describe('Test utils.check_type:', function()
+  it("should have no error", function()
+    local var = 'string'
+    assert.has_no.errors(function() u.check_type(var, 'string') end)
+  end)
+
+  it("should have no error", function()
+    local var = nil
+    assert.has_no.errors(function() u.check_type(var, 'nil') end)
+  end)
+
+  it("should have no error", function()
+    local var = nil
+    assert.has_no.errors(function() u.check_type(var, 'table', true) end)
+  end)
+
+  it("should have error", function()
+    local var = 'string'
+    assert.has.errors(function() u.check_type(var, 'table') end)
+  end)
+
+  it("should have error", function()
+    local var = nil
+    assert.has.errors(function() u.check_type(var, 'table') end)
+  end)
+end)
