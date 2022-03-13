a410413f98274ebc404d7124d4bffe30b2271db0
ZhiyuanLck <lichangkai225@qq.com>
2022-03-13 14:18:18 +0800

    refactor: basic setup
    - Complete basic functions of `Pairs.setup`.
    - Add corresponding tests.

--- lua/pairs/init.lua
+++ lua/pairs/init.lua
@@ -1,22 +1,29 @@
-local u = require("pairs.utils")
+local u = require('pairs.utils')
+local get_config = require('pairs.config').get_config
 
 --- Pairs class
 ---@class Pairs
 local Pairs = {}
 
---- setup configuration
+--- setup configuration and keymap
 ---@vararg table @user configuration
 function Pairs.setup(...)
   local arg = {...}
   local user_config
 
   if arg[1] == Pairs then
-    u.warn([[`require("pairs"):setup()` is deprecated, use `require("pairs").setup()` instead]])
+    u.warn([[`require('pairs'):setup()` is deprecated, use `require('pairs').setup()` instead]])
     user_config = arg[2] or {}
   else
     user_config = arg[1] or {}
   end
 
+  u.check_type(user_config, 'table', true)
+
+  local config = get_config(user_config)
+  for k, v in pairs(config) do
+    Pairs[k] = v
+  end
 end
 
 return Pairs
--- tests/pairs/config_spec.lua
+++ tests/pairs/config_spec.lua
@@ -1,10 +1,15 @@
 local conf = require('pairs.config')
+local Pairs = require('pairs')
 
-describe('Test configuration:', function()
+describe('Test configuration of pairs:', function()
   it("should use the default html pairs", function()
     local config = conf.get_config()
     assert.are.same('<', config.pairs.html[1].left)
     assert.are.same('>', config.pairs.html[1].right)
+
+    Pairs.setup()
+    assert.are.same('<', Pairs.pairs.html[1].left)
+    assert.are.same('>', Pairs.pairs.html[1].right)
   end)
 
   it("custom html pairs should overwrite the default", function()
@@ -16,6 +21,10 @@ describe('Test configuration:', function()
     local config = conf.get_config(user_config)
     assert.are.same('a', config.pairs.html[1].left)
     assert.are.same('b', config.pairs.html[1].right)
+
+    Pairs.setup(user_config)
+    assert.are.same('a', Pairs.pairs.html[1].left)
+    assert.are.same('b', Pairs.pairs.html[1].right)
   end)
 
   it("custom html pairs should be added", function()
@@ -27,5 +36,9 @@ describe('Test configuration:', function()
     local config = conf.get_config(user_config)
     assert.are.same('a', config.pairs.rust[1].left)
     assert.are.same('b', config.pairs.rust[1].right)
+
+    Pairs.setup(user_config)
+    assert.are.same('a', Pairs.pairs.rust[1].left)
+    assert.are.same('b', Pairs.pairs.rust[1].right)
   end)
 end)
