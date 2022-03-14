3cf2ababcf4c5ae7c3f03793e52971de55408064
ZhiyuanLck <lichangkai225@qq.com>
2022-03-14 18:56:56 +0800

    refactor: add class Region

---
+++ lua/pairs/region.lua
@@ -0,0 +1,31 @@
+local u = require('pairs.utils')
+
+---@class Region
+---@field start string @start of the region
+---@field finish string @end of the region
+---@field cross_line boolean @whether the region can cross lines
+---@field priority number @default 0
+local Region = {}
+
+--- create a region object
+---@param region Region
+---@return Region
+function Region.new(region)
+  u.check_type(region, 'table')
+  u.check_type(region.start, 'string')
+  u.check_type(region.finish, 'string', true)
+  u.check_type(region.cross_line, 'boolean', true)
+  u.check_type(region.priority, 'number', true)
+
+  if region.finish == nil then
+    region.cross_line = false
+  else
+    region.cross_line = u.if_nil(region.cross_line, region.start ~= region.finish)
+  end
+
+  region.priority = u.if_nil(region.priority, 0)
+
+  return setmetatable(region, Region)
+end
+
+return Region
---
+++ tests/pairs/region_spec.lua
@@ -0,0 +1,39 @@
+local Region = require('pairs.region')
+
+describe("Test class 'Region':", function()
+  it("should have error when start is nil", function()
+    assert.has.errors(function() Region.new({}) end)
+  end)
+
+  it("should have error when start is not string", function()
+    assert.has.errors(function() Region.new{start = 2} end)
+  end)
+
+  it("should have error when finish is not string", function()
+    assert.has.errors(function() Region.new{start = '', finish = {}} end)
+  end)
+
+  it("should have error when cross_line is not boolean", function()
+    assert.has.errors(function() Region.new{start = '', cross_line = 2} end)
+  end)
+
+  it("should have error when priority is not number", function()
+    assert.has.errors(function() Region.new{start = '', priority = ''} end)
+  end)
+
+  it("cross_line should be false if finish is nil", function()
+    assert.is_not_true(Region.new{start = ''}.cross_line)
+  end)
+
+  it("cross_line should be false if start is equal to finish", function()
+    assert.is_not_true(Region.new{start = 'a', finish = 'a'}.cross_line)
+  end)
+
+  it("cross_line should be true if start is not equal to finish", function()
+    assert.is_true(Region.new{start = 'a', finish = 'b'}.cross_line)
+  end)
+
+  it("priority should be 0", function()
+    assert.are.same(0, Region.new{start = ''}.priority)
+  end)
+end)
