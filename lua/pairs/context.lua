25e1ba212a11b7e39628c09e4095b58a858ef893
ZhiyuanLck <lichangkai225@qq.com>
2022-03-14 13:16:38 +0800

    refactor: add counters
    - Pass opts to `Context:init`
    - Add class `Counter`
    - Add `Context.count_left` and `Context.count_right`

--- lua/pairs/context.lua
+++ lua/pairs/context.lua
@@ -20,20 +20,94 @@ local ctx_opts = {}
 --- create the Context object
 ---@param opts CtxOpts
 ---@return Context
-function Context:new(opts)
+function Context.new(opts)
   local ctx = setmetatable({}, Context)
-  ctx:init()
+  ctx:init(opts)
   return ctx
 end
 
 --- initialization code when a new Context object is created
-function Context:init()
+---@param opts CtxOpts
+function Context:init(opts)
   local cursor = u.get_cursor()
   self.cur_line_idx = cursor[1]
   self.cur_col_idx = cursor[2]
   self.cur_line = api.nvim_get_current_line()
   self.cur_left = fn.strpart(self.cur_line, 0, self.cur_col_idx)
   self.cur_right = fn.strpart(self.cur_line, self.cur_col_idx)
+end
+
+--- bracket counters
+---@class Counter
+---@field real number @real amount
+---@field valid number @valid amount with the minimum 0
+local Counter = {}
+Counter.__index = Counter
+
+--- create a counter object
+---@return Counter
+function Counter.new()
+  return setmetatable({real = 0, valid = 0}, Counter)
+end
+
+--- increase the counter
+function Counter:incr()
+  self.real  = self.real + 1
+  self.valid = self.valid + 1
+end
+
+--- decrease the counter
+function Counter:decr()
+  self.real = self.real - 1
+  self.valid = self.valid == 0 and 0 or self.valid - 1
+end
+
+--- calculate the amount of the left brackets relative to the right brackets
+---@param str string @input text
+---@param left string @left bracket
+---@param right string @right bracket
+---@param ctn Counter | nil @optional continuous counter
+---@return Counter
+function Context.count_left(str, left, right, ctn)
+  local cur = 1
+  local ln, rn, sn = #left, #right, #str
+  ctn = ctn or Counter.new()
+  repeat
+    if str:sub(cur, cur + ln - 1) == left then
+      ctn:incr()
+      cur = cur + ln
+    elseif str:sub(cur, cur + rn - 1) == right then
+      ctn:decr()
+      cur = cur + rn
+    else
+      cur = cur + 1
+    end
+  until (cur > sn)
+  return ctn
+end
+
+--- calculate the amount of the right brackets relative to the left brackets
+---@param str string @input text
+---@param left string @left bracket
+---@param right string @right bracket
+---@param ctn Counter | nil @optional continuous counter
+---@return Counter
+function Context.count_right(str, left, right, ctn)
+  local ln, rn, sn = #left, #right, #str
+  local cur = sn
+  ctn = ctn or Counter.new()
+  repeat
+    if str:sub(cur - rn + 1, cur) == right then
+      ctn:incr()
+      cur = cur - rn
+    elseif str:sub(cur - ln + 1, cur) == left then
+      ctn:decr()
+      cur = cur - ln
+    else
+      cur = cur - 1
+    end
+  until (cur < 0)
+  return ctn
 end
 
 return Context
