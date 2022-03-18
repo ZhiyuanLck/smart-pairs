1d3e16f9c95d5377718c2d4aefddde678645b16a
ZhiyuanLck <lichangkai225@qq.com>
2022-03-18 14:48:03 +0800

    add Stack data structure

---
+++ lua/pairs/stack.lua
@@ -0,0 +1,39 @@
+---@class Stack
+---@filed size
+local Stack = {}
+Stack.__index = Stack
+
+--- creat a new stack
+---@return Stack
+function Stack.new()
+  return setmetatable({size = 0}, Stack)
+end
+
+--- push data to stack
+---@param data any
+function Stack:push(data)
+  self.size = self.size + 1
+  self[self.size] = data
+end
+
+--- pop the top item and return it
+---@return any
+function Stack:pop()
+  if self.size == 0 then
+    error('cannot pop from an empty stack')
+  end
+  local item = self[self.size]
+  self.size = self.size - 1
+  return item
+end
+
+--- get the top item of the stack
+---@return any
+function Stack:top()
+  if self.size == 0 then
+    error('cannot get the top item from an empty stack')
+  end
+  return self[self.size]
+end
+
+return Stack
